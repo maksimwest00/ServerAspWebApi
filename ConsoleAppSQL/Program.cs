6fc6cf1aa6b6b9411ec896bf4dd50a24a6235d05
Максим Сухарев <m.sukharev@trekmark.ru>
2024-09-09 15:58:45 +0300

    чистка проектов

--- ConsoleAppSQL/Program.cs
+++ ConsoleAppSQL/Program.cs
@@ -10,29 +10,6 @@ namespace ConsoleAppSQL
         static void Main(string[] args)
         {
             Console.WriteLine("ConsoleAppSQL!");
-
-            var dbService = new DataBaseService();
-            var patientEnv = new PatientTableEnviroment(dbService);
-            // EDIT - РАБОТАЕТ
-            // DELETE - РАБОТАЕТ
-            // ADD - РАБОТАЕТ
-            // GETBYID - РАБОТАЕТ
-            // GET BY PAGE AND SORT - РАБОТАЕТ
-
-            //Task.Run(async () =>
-            //{
-            //    string[] columnsNames = await patientEnv.GetColumnNameInTable("Пациенты");
-            //    var a = 1;
-            //});
-
-
-            (patientEnv.GetListByPageAndSort(1, "Id")).ContinueWith((patient) =>
-            {
-                var fasjfjas = patient.Result;
-                var asdkaskd = 1;
-            });
-
-            var b = Console.ReadLine();
         }
     }
 }
